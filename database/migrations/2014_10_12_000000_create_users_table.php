6912da034e6d4e7e1f05b0ee6f40b695a54b96aa
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-18 23:09:54 +0300

    create users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,13 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->decimal('balance', 15, 4)->default('0');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('api_key')->nullable();
+            $table->string('status')->default('ACTIVE');
+            $table->string('timezone');
+            $table->string('activation_token')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
