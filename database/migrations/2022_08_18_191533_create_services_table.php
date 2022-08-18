c24600370df6f6a289ff9469fd385335d4199217
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-18 23:10:03 +0300

    create services table

---
+++ database/migrations/2022_08_18_191533_create_services_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->text('name');
+            $table->text('description');
+            $table->decimal('price', 15, 2)->default('0');
+            $table->integer('min');
+            $table->integer('max');
+            $table->string('type')->default('manual');
+            $table->integer('api_provider_id')->nullable()->default(NULL);
+            $table->integer('api_service_id')->nullable()->default(NULL);
+            $table->string('status')->default('PENDING');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+};
