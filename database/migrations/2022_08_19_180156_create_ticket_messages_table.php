c7f9e3530e8f717cef99a49fcaed9562b281dca6
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-19 22:34:06 +0300

    create ticket_messages table

---
+++ database/migrations/2022_08_19_180156_create_ticket_messages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_messages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('ticket_id');
+            $table->integer('user_id');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_messages');
+    }
+};
