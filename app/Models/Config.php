731ad4cf1e01edea62a41b4fc008100eb733b886
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-21 01:13:35 +0300

    create config model

---
+++ app/Models/Config.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Config
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $value
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Config newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Config newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Config query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Config whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Config whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Config whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Config whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Config whereValue($value)
+ * @mixin \Eloquent
+ */
+class Config extends Model
+{
+
+    use HasFactory;
+
+    protected $guarded = ['id', 'name'];
+
+}
