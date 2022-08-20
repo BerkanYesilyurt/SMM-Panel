c4529d09b920bfb10b0e0f27897ce8f7b7a724a2
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-21 01:13:08 +0300

    index defined

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Config;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public function check(Config $config){
+    $maintenance_mode = $config->where('name','=', 'maintenance_mode')->value('value');
+    $directly_login = $config->where('name','=', 'directly_login')->value('value');
+
+    if($maintenance_mode){
+        return view('undermaintenance');
+    }
+
+    if(auth()){
+        return view('dashboard');
+    }elseif($directly_login){
+        return route('login');
+    }else{
+        return view('index');
+    }
+
+    }
+
+
     public function showLogin(){
         return view('login');
     }
