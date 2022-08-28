f2500f771e967f991f100c38703a8ea46a5266a0
BerkanYesilyurt <berkan38212@gmail.com>
2022-08-29 00:17:38 +0300

    new providers added

--- config/app.php
+++ config/app.php
@@ -181,6 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        App\Providers\ConfigServiceProvider::class,
+        App\Providers\AnnouncementServiceProvider::class,
 
         /*
          * Package Service Providers...
