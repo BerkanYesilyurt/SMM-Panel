cc888219689bc3e4e695ba616909a5b042c91869
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-09 22:57:15 +0300

    create ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function profilePage(){
+
+    }
+
+    public function updateProfile(){
+
+    }
+}
