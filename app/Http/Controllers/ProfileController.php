2e6f780bcf7e2d7c891770161e71cbb3be409d92
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-10 23:42:30 +0300

    profilePage func

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,15 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function profilePage(){
-
+    public function profilePage(User $user){
+        $currentUser = $user::findOrFail(auth()->user()->id);
+        return view('pages.profile', compact('currentUser'));
     }
 
     public function updateProfile(){
+
+    }
+
+    public function generateToken(){
 
     }
 }
