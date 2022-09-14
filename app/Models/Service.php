2c1b51996f43405c9413b13ce7208bea4e0e09c0
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-14 20:31:40 +0300

    create service model

---
+++ app/Models/Service.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Service
+ *
+ * @property int $id
+ * @property int $category_id
+ * @property string $name
+ * @property string $description
+ * @property string $price
+ * @property int $min
+ * @property int $max
+ * @property string $type
+ * @property int|null $api_provider_id
+ * @property int|null $api_service_id
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Service newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Service newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Service query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereApiProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereApiServiceId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereMax($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereMin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Service whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Service extends Model
+{
+    use HasFactory;
+
+    protected $table = 'services';
+
+    protected $guarded = [];
+
+}
