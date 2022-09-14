0d613f8afffe5c1e0f5417a1c17e7a180cdf6762
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-15 00:25:40 +0300

    servicesPage func

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function servicesPage(Service $service, Category $category){
+        $services = $service->all();
+        $categories = $category->all();
+        return view('pages.services', compact('services', 'categories'));
+    }
 }
