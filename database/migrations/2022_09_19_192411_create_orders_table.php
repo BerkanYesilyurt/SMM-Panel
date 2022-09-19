1c7cd6673986d5bbd7b21e3241fe15e1b7b0da87
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-20 00:29:36 +0300

    orders table

--- database/migrations/2022_09_19_192411_create_orders_table.php
+++ database/migrations/2022_09_19_192411_create_orders_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('service_id');
+            $table->text('link');
+            $table->integer('quantity');
+            $table->decimal('charge', 15, 5);
+            $table->string('type')->default('manual');
+            $table->integer('api_provider_id')->nullable()->default(NULL);
+            $table->integer('api_service_id')->nullable()->default(NULL);
+            $table->integer('api_order_id')->nullable()->default(NULL);
+            $table->string('status')->default('PENDING');
             $table->timestamps();
         });
     }
