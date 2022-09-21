777ef0f4da03171828c0a09c7f205fa85f083c6b
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-21 20:40:14 +0300

    orderPage func

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function orderPage(){
+        $categories = [];
+        $services = [];
+
+        foreach(Category::all() as $category){
+            $categories[$category->id] = $category->name;
+            $services[$category->id] = Category::find($category->id)->getServicesWithCategory();
+        }
+
+        return view('pages.new-order', compact('services', 'categories'));
+    }
 }
