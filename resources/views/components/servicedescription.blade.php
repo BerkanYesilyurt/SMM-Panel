84bfd978a978438224ca7d496bb799d4dc46e1d0
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-25 00:15:01 +0300

    trim & data changes

--- resources/views/components/servicedescription.blade.php
+++ resources/views/components/servicedescription.blade.php
@@ -4,9 +4,9 @@
     var servicesList = [
 @foreach($services as $service)
         @php
-            preg_match_all('/{refill}(.*?){\/refill}/s', $service[0]->description, $refill);
-            preg_match_all('/{starttime}(.*?){\/starttime}/s', $service[0]->description, $starttime);
-            preg_match_all('/{speed}(.*?){\/speed}/s', $service[0]->description, $speed);
+            preg_match_all('/{refill}(.*?){\/refill}/s', $service->description, $refill);
+            preg_match_all('/{starttime}(.*?){\/starttime}/s', $service->description, $starttime);
+            preg_match_all('/{speed}(.*?){\/speed}/s', $service->description, $speed);
 
             $refillExp = array_key_exists(0, $refill) ? (array_key_exists(0, $refill[0]) ? $refill[0][0] : '') : '';
             $starttimeExp = array_key_exists(0, $starttime) ? (array_key_exists(0, $starttime[0]) ? $starttime[0][0] : '') : '';
@@ -18,9 +18,9 @@
             $speedExpFull = str_replace($tagsManual, '', $speedExp);
 
             $tags = [$refillExp, $starttimeExp, $speedExp];
-            $newDescription = str_replace($tags, '', $service[0]->description);
+            $newDescription = trim(str_replace($tags, '', $service->description));
         @endphp
-        {id:{{$service[0]->id}}, name:"{{$service[0]->name}}", refill:"{{$refillExpFull}}", starttime:"{{$starttimeExpFull}}", speed:"{{$speedExpFull}}", description:"{{$newDescription}}"},
+        {id:{{$service->id}}, name:"{{$service->name}}", refill:"{{$refillExpFull}}", starttime:"{{$starttimeExpFull}}", speed:"{{$speedExpFull}}", description:"{{$newDescription}}"},
 @endforeach
     ];
 </script>
