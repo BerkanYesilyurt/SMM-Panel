16ab18035c1d2db4f2f5c3190edc48038d3c3168
BerkanYesilyurt <berkan38212@gmail.com>
2022-09-28 20:17:59 +0300

    LastLoginListener

---
+++ app/Listeners/LastLoginListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\LastLogin;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LastLoginListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\LastLogin  $event
+     * @return void
+     */
+    public function handle(LastLogin $event)
+    {
+        $event->user->last_login = now();
+        $event->user->last_login_ip = $event->request->ip();
+        $event->user->save();
+    }
+}
