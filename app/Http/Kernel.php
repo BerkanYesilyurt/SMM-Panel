b96f72c6149c5e61e1444d9d7aa73a22a60a02a1
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-16 23:21:11 +0300

    new middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'maintenance' => \App\Http\Middleware\Maintenance::class,
-        'verifypanelinstalled' => \App\Http\Middleware\VerifyPanelInstalled::class
+        'verifypanelinstalled' => \App\Http\Middleware\VerifyPanelInstalled::class,
+        'isadmin' => \App\Http\Middleware\isAdmin::class
     ];
 }
