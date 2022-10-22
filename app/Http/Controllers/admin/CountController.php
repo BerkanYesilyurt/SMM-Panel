858f36023dd7c4c0f71db5583b048f716a1932ec
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-23 01:36:11 +0300

    count all records

--- app/Http/Controllers/admin/CountController.php
+++ app/Http/Controllers/admin/CountController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcement;
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\Service;
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class CountController extends Controller
 {
-    public array $count;
+    public array $count = [];
 
     public function countAll(){
         foreach(get_class_methods($this) as $class){
@@ -17,5 +23,34 @@ class CountController extends Controller
             }
         }
         return $this->count;
+    }
+
+    private function countUsers(){
+        $this->count['users'] = User::count();
+    }
+
+    private function countTickets(){
+        $this->count['tickets'] = Ticket::count();
+        $this->count['opentickets'] = Ticket::whereNot('status', '=', 'CLOSED')->count();
+    }
+
+    private function countOrders(){
+        $this->count['orders'] = Order::count();
+    }
+
+    private function countServices(){
+        $this->count['services'] = Service::count();
+    }
+
+    private function countCategories(){
+        $this->count['categories'] = Category::count();
+    }
+
+    private function countAnnouncements(){
+        $this->count['announcements'] = Announcement::count();
+    }
+
+    private function countRevenue(){
+        $this->count['revenue'] = Order::whereNot('status','=','CANCELED')->sum('charge');
     }
 }
