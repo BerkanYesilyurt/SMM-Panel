1f7e4f561b70b4f4a45d21a5a9ee59b4f7dee3c9
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-23 01:36:47 +0300

    dashboardPage func

--- app/Http/Controllers/admin/DashboardController.php
+++ app/Http/Controllers/admin/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Http\Controllers\ConfigController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function dashboardPage(){
-        return view('pages.admin.dashboard');
+        $count = new CountController();
+        return view('pages.admin.dashboard', [
+            'count' => $count->countAll(),
+            'config' => ConfigController::configs()
+        ]);
     }
 }
