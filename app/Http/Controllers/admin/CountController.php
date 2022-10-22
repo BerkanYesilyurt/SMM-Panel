dab374214e054e0a678a0b7530f5d98610154799
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-22 19:44:45 +0300

    countAll func

--- app/Http/Controllers/admin/CountController.php
+++ app/Http/Controllers/admin/CountController.php
@@ -4,7 +4,18 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CountController extends Controller
 {
+    public array $count;
+
+    public function countAll(){
+        foreach(get_class_methods($this) as $class){
+            if($class != 'countAll' && Str::startsWith($class, 'count')){
+               $this->$class();
+            }
+        }
+        return $this->count;
+    }
 }
