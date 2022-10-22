0fe5227310a1ac9fb0435c6d4899cb076385cac3
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-23 01:38:02 +0300

    general stats of panel

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,5 +1,93 @@
 @extends('pages.admin.layout')
 @section('subTitle', 'Dashboard')
 @section('content')
+    <!-- Content wrapper -->
+    <div class="content-wrapper">
+        <!-- Content -->
 
+        <div class="container-xxl flex-grow-1 container-p-y">
+            <div class="row">
+                <div class="col-lg-12 col-md-4 order-1">
+                    <div class="row">
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Users:</span>
+                                    <h3 class="card-title mb-2">{{$count['users']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Orders:</span>
+                                    <h3 class="card-title mb-2">{{$count['orders']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Revenue:</span>
+                                    <h3 class="card-title mb-2">{{$config['currency_symbol']}}{{number_format($count['revenue'],2,",",".")}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Announcements:</span>
+                                    <h3 class="card-title mb-2">{{$count['announcements']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Tickets:</span>
+                                    <h3 class="card-title mb-2">{{$count['tickets']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Open Tickets:</span>
+                                    <h3 class="card-title mb-2">{{$count['opentickets']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Categories:</span>
+                                    <h3 class="card-title mb-2">{{$count['categories']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-12 col-6 mb-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <span class="fw-semibold d-block mb-1">Services:</span>
+                                    <h3 class="card-title mb-2">{{$count['services']}}</h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
 @endsection
