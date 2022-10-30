c12459d0314e04420e6a588b436cab23487b2044
BerkanYesilyurt <berkan38212@gmail.com>
2022-10-30 19:25:45 +0300

    auto-login option & additional settings

--- resources/views/pages/admin/system-settings.blade.php
+++ resources/views/pages/admin/system-settings.blade.php
@@ -95,10 +95,17 @@
                             </select>
                         </div>
                         <div class="mb-3 col-md-6">
-                            <label for="directly_login" class="form-label">Homepage Directly Login</label>
+                            <label for="directly_login" class="form-label">Default Homepage: Login Page</label>
                             <select name="directly_login" id="directly_login" class="select2 form-select">
                                 <option value="0" @selected($settings['directly_login']['value'] == 0)>Inactive</option>
                                 <option value="1" @selected($settings['directly_login']['value'] == 1)>Active</option>
+                            </select>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="forgot_password" class="form-label">Auto-login After Registration</label>
+                            <select name="forgot_password" id="forgot_password" class="select2 form-select">
+                                <option value="0" @selected($settings['autologin_after_registration']['value'] == 0)>Inactive</option>
+                                <option value="1" @selected($settings['autologin_after_registration']['value'] == 1)>Active</option>
                             </select>
                         </div>
                         <div class="mb-3 col-md-6">
@@ -116,6 +123,59 @@
                 </form>
             </div>
         </div>
+
+        <div class="card mb-4">
+            <h5 class="card-header">Additional</h5>
+            <div class="card-body">
+                <form action="/admin/system-settings" method="POST">
+                    @csrf
+                    <input type="hidden" name="type" value="secondsection" />
+                    <div class="row">
+                        <div class="mb-3 col-md-6">
+                            <label for="terms_content" class="form-label">Terms</label>
+                            <textarea class="form-control" name="terms_content" id="terms_content" rows="6">{{$settings['terms_content']['value']}}</textarea>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="policy_content" class="form-label">Policy</label>
+                            <textarea class="form-control" name="policy_content" id="policy_content" rows="6">{{$settings['policy_content']['value']}}</textarea>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="javascript_embed_header" class="form-label">Javascript Embed Header</label>
+                            <textarea class="form-control" name="javascript_embed_header" id="javascript_embed_header" rows="6">{{$settings['javascript_embed_header']['value']}}</textarea>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="javascript_embed_footer" class="form-label">Javascript Embed Footer</label>
+                            <textarea class="form-control" name="javascript_embed_footer" id="javascript_embed_footer" rows="6">{{$settings['javascript_embed_footer']['value']}}</textarea>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="facebook_link" class="form-label">Facebook Link</label>
+                            <input class="form-control" type="text" id="facebook_link" name="facebook_link"
+                                   value="{{$settings['facebook_link']['value']}}">
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="twitter_link" class="form-label">Twitter Link</label>
+                            <input class="form-control" type="text" id="twitter_link" name="twitter_link"
+                                   value="{{$settings['twitter_link']['value']}}">
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="instagram_link" class="form-label">Instagram Link</label>
+                            <input class="form-control" type="text" id="instagram_link" name="instagram_link"
+                                   value="{{$settings['instagram_link']['value']}}">
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="youtube_link" class="form-label">Youtube Link</label>
+                            <input class="form-control" type="text" id="youtube_link" name="youtube_link"
+                                   value="{{$settings['youtube_link']['value']}}">
+                        </div>
+
+                        <div class="mt-2">
+                            <button type="submit" class="btn btn-primary me-2">Change System Settings</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
     </div>
 </div>
 @endsection
