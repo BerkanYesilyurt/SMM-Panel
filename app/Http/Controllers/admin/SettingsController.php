31bb685e0fb118933cacc09be5db7840bdb27eee
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-03 00:17:37 +0300

    refactor

--- app/Http/Controllers/admin/SettingsController.php
+++ app/Http/Controllers/admin/SettingsController.php
@@ -32,31 +32,8 @@ class SettingsController extends Controller
         if ($request->type == 'maintenancemode'){
             $config->where('name', '=', 'maintenance_mode')->update(['value' => DB::raw('!value')]);
             $changedSettingNames[] = 'Maintenance Mode';
-        }elseif($request->type == 'firstsection'){
-            $changedSettingNames = $this->setSystemSettingWith([
-                'title' => $request->title,
-                'meta_description' => $request->meta_description,
-                'meta_keywords' => $request->meta_keywords,
-                'currency' => $request->currency,
-                'currency_symbol' => $request->currency_symbol,
-                'service_updates_page' => $request->service_updates_page,
-                'register_page' => $request->register_page,
-                'directly_login' => $request->directly_login,
-                'autologin_after_registration' => $request->autologin_after_registration,
-                'forgot_password' => $request->forgot_password,
-            ], $request->attributes());
-        }elseif($request->type == 'secondsection'){
-            $changedSettingNames = $this->setSystemSettingWith([
-                'terms_content' => $request->terms_content ?? null,
-                'policy_content' => $request->policy_content ?? null,
-                'javascript_embed_header' => $request->javascript_embed_header ?? null,
-                'javascript_embed_footer' => $request->javascript_embed_footer ?? null,
-                'facebook_link' => $request->facebook_link,
-                'twitter_link' => $request->twitter_link,
-                'instagram_link' => $request->instagram_link,
-                'youtube_link' => $request->youtube_link,
-            ], $request->attributes());
-
+        }elseif($request->type == 'firstsection' || $request->type == 'secondsection'){
+            $changedSettingNames = $this->setSystemSettingWith($request->validated(), $request->attributes());
         }else{
             abort(416);
         }
