c0b5bc52e6c69b091fbd4355e4856bb59c170b1a
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-05 23:01:04 +0300

    dates added

--- app/Models/User.php
+++ app/Models/User.php
@@ -84,4 +84,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'email_verified_at',
+        'last_login'
+    ];
 }
