48c5526104b32bb00cee9b75f96844fb79bf1205
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-10 23:02:15 +0300

    updateUserDetails func & updateUserBalance func

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\ConfigController;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\admin\UpdateUserDetailsRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -20,7 +21,20 @@ class UserController extends Controller
         return view('pages.admin.user', compact('user', 'config'));
     }
 
-    public function updateUserDetails(User $user){
+    public function updateUserDetails(UpdateUserDetailsRequest $request, User $user){
+        $user->update($request->validated());
+        return back()->with('message', 'You have successfully edited user details.');
+    }
 
+    public function updateUserBalance(Request $request, User $user){
+        $request->validate([
+            'balance' => 'required|numeric|min:0|digits_between:1,10'
+        ]);
+
+        $user->update([
+            'balance' => $request->balance
+        ]);
+
+        return back()->with('message', 'You have successfully updated user balance.');
     }
 }
