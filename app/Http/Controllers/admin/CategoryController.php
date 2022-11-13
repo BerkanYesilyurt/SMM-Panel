22fead5d7ffe9216892face76806e0e2d9833dbf
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-14 00:03:06 +0300

    updateCategory func

--- app/Http/Controllers/admin/CategoryController.php
+++ app/Http/Controllers/admin/CategoryController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Enums\CategoryStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Enum;
 
 class CategoryController extends Controller
 {
@@ -13,5 +15,21 @@ class CategoryController extends Controller
         return view('pages.admin.categories', [
             'categories' => Category::all()
         ]);
+    }
+
+    public function updateCategory(Request $request)
+    {
+        $request->validate([
+            'id' => 'required|numeric',
+            'name' => 'required|min:1|max:250',
+            'status' => ['required', new Enum(CategoryStatusEnum::class)]
+        ]);
+
+        Category::findOrFail($request->id)->update([
+            'name' => $request->name,
+            'status' => $request->status
+        ]);
+
+        return back()->with('message', 'You have successfully edited category details.');
     }
 }
