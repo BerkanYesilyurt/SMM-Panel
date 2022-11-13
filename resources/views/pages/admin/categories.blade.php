60ac6d4d15685e079cc642448ffd4896ffc6459c
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-13 19:29:06 +0300

    change category details & error success messages

--- resources/views/pages/admin/categories.blade.php
+++ resources/views/pages/admin/categories.blade.php
@@ -5,27 +5,52 @@
         <h4 class="fw-bold py-3 mb-4">
             <span class="text-muted fw-light">Admin Panel /</span> Categories
         </h4>
-
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul style="margin-bottom: 1px;">
+                    @foreach ($errors->all() as $error)
+                        <li><b>{{$error}}</b></li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if(session('message'))
+            <div class="alert alert-success alert-dismissible">
+                <ul style="margin-bottom: 1px; color:#478924;">
+                    <b>{{session('message')}}</b>
+                </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                </button>
+            </div>
+        @endif
         <div class="card">
 
             <div class="table-responsive text-nowrap">
+                <style>
+                    table {
+                        border-spacing: 0px;
+                        table-layout: fixed;
+                        margin-left: auto;
+                        margin-right: auto;
+                    }
+                </style>
                 <table class="table">
                     <thead>
                     <tr>
-                        <th><center>CATEGORY ID</center></th>
-                        <th><center>NAME</center></th>
-                        <th><center>STATUS</center></th>
-                        <th><center>ACTIONS</center></th>
+                        <th style="width: 10%;"><center>CATEGORY ID</center></th>
+                        <th style="width: 50%;"><center>NAME</center></th>
+                        <th style="width: 20%;"><center>STATUS</center></th>
+                        <th style="width: 20%;"><center>ACTIONS</center></th>
                     </tr>
                     </thead>
                     <tbody class="table-border-bottom-0">
                     @forelse($categories as $category)
                         <tr>
                             <td><center><b>{{$category->id}}</b></center></td>
-                            <td><center>{{$category->name}}</center></td>
-                            <td><center>{{$category->status}}</center></td>
+                            <td style="white-space:pre-wrap; word-wrap:break-word;"><center>{{$category->name}}</center></td>
+                            <td><center><span class="badge bg-{{$category->status == \App\Enums\CategoryStatusEnum::ACTIVE->value ? 'success' : 'danger'}} me-1">{{$category->status == \App\Enums\CategoryStatusEnum::ACTIVE->value ? 'ACTIVE' : 'INACTIVE'}}</span></center></td>
                             <td><center><button type="button" onclick="changeModal(this)" data-categoryid="{{$category->id}}" data-categoryname="{{$category->name}}" data-categorystatus="{{$category->status}}" class="btn btn-sm btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenter">
-                                        <span class="tf-icons bx bx-detail"></span>&nbsp; Change Details
+                                        <span class="tf-icons bx bx-pencil"></span>&nbsp; Change Details
                                     </button></center>
                             </td>
                         </tr>
@@ -54,26 +79,42 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="row">
+                    <form method="POST" id="form" action="/admin/categories">
+                        @csrf
+                        <input type="hidden" name="id" id="id">
 
-                        <div class="col mb-3" id="modalCategoryName">
-                            <b>Loading..</b>
+                        <div class="col mb-3">
+                            <label for="name" class="form-label">Name:</label>
+                            <textarea class="form-control" rows="5" maxlength="250" name="name" id="name" style="width: 100%; resize: none;" aria-label="With textarea"></textarea>
                         </div>
-                    </div>
 
-                    <div class="row g-2">
-                        <div class="col mb-3" id="modalCategoryStatus">
-                            <b>Loading..</b>
+                        <div class="col mb-3">
+                            <label for="status" class="form-label">Status:</label>
+                            <select id="status" class="form-control" name="status">
+                                @foreach(App\Enums\CategoryStatusEnum::values() as $key => $value)
+                                <option value="{{$key}}">{{$value}}</option>
+                                @endforeach
+                            </select>
                         </div>
-                    </div>
+
+                        <div class="col mb-3">
+                            <button class="btn btn-primary" onclick="submit(); this.disabled = true;" style="color: white; width: 100%;" id="submitbutton">Update Category</button>
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
     </div>
     <script>
         function changeModal(element){
-            document.getElementById('modalCategoryName').innerHTML = '<h5><b>' + element.dataset.categoryname + ' (ID: ' + element.dataset.categoryid + ')</b></h5>';
-            document.getElementById('modalCategoryStatus').innerHTML = element.dataset.categorystatus;
+            document.getElementById('id').value = element.dataset.categoryid;
+            document.getElementById('name').value = element.dataset.categoryname;
+            document.getElementById('status').value = element.dataset.categorystatus;
+        }
+
+        function submit(){
+            document.getElementById("form").submit();
         }
     </script>
 @endsection
