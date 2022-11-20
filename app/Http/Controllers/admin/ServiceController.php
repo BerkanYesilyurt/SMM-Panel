10044c913a9a413f2954d72ec3443f2e99d87e30
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-20 19:48:09 +0300

    servicesPage func

--- app/Http/Controllers/admin/ServiceController.php
+++ app/Http/Controllers/admin/ServiceController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function servicesPage()
+    {
+        return view('pages.admin.services', [
+            'services' => Service::all()
+        ]);
+    }
 }
