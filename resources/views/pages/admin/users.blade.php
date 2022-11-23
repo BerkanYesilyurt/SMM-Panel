bbf84e6d0ebe44913b5004a63bf21833a7c90824
BerkanYesilyurt <berkan38212@gmail.com>
2022-11-24 00:05:36 +0300

    use UserStatusEnum

--- resources/views/pages/admin/users.blade.php
+++ resources/views/pages/admin/users.blade.php
@@ -52,7 +52,7 @@
                         echo 'success';
                     }
                     @endphp me-1">{{$user->authority == \App\Enums\UserAuthorityEnum::none->value ? 'USER' : strtoupper(\App\Enums\UserAuthorityEnum::from($user->authority)->name)}}</span></center></td>
-                            <td><center>{{$user->status}}</center></td>
+                            <td><center>{{\App\Enums\UserStatusEnum::from($user->status)->name}}</center></td>
                             <td><center>{{$configsArray['currency_symbol']}}{{round($user->balance, 4)}}</center></td>
                             <td><center>{{$user->created_at->diffForHumans()}}</center></td>
                             <td><center>
