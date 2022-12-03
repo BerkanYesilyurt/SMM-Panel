4cce73e4d2e72e00da9d9c0637cc65b9f57328bf
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-04 00:52:15 +0300

    announcementsPage func

--- app/Http/Controllers/admin/AnnouncementController.php
+++ app/Http/Controllers/admin/AnnouncementController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Announcement;
 use Illuminate\Http\Request;
 
 class AnnouncementController extends Controller
 {
-    //
+    public function announcementsPage()
+    {
+        return view('pages.admin.announcements', [
+            'announcements' => Announcement::all()
+        ]);
+    }
 }
