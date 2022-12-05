5e13f09e001affeb5dea871028462a003296deee
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-06 00:10:36 +0300

    updateAnnouncement & createNewAnnouncement func

--- app/Http/Controllers/admin/AnnouncementController.php
+++ app/Http/Controllers/admin/AnnouncementController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use App\Models\Announcement;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class AnnouncementController extends Controller
 {
@@ -15,13 +16,27 @@ class AnnouncementController extends Controller
         ]);
     }
 
-    public function updateAnnouncement(Request $request)
+    public function updateAnnouncement(Request $request, Announcement $announcement)
     {
-        //TODO
+        $fields = $request->validate([
+            'id' => 'required|numeric|exists:announcements,id',
+            'title' => 'required|min:1|max:1000',
+            'description' => 'required|min:1|max:1000'
+        ]);
+
+        $filteredFields = Arr::except($fields, ['id']);
+        $announcement->findOrFail($request->id)->update($filteredFields);
+        return back()->with('message', 'You have successfully updated the announcement.');
     }
 
     public function createNewAnnouncement(Request $request)
     {
-        //TODO
+        $fields = $request->validate([
+            'title' => 'required|min:1|max:1000',
+            'description' => 'required|min:1|max:1000'
+        ]);
+
+        Announcement::create($fields);
+        return back()->with('message', 'You have successfully created an announcement.');
     }
 }
