cfef69aa8618827a8834f5c31aae4041188c7e25
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-10 22:49:52 +0300

    service updates' actions

--- resources/views/pages/admin/serviceupdates.blade.php
+++ resources/views/pages/admin/serviceupdates.blade.php
@@ -5,6 +5,25 @@
         <h4 class="fw-bold py-3 mb-4">
             <span class="text-muted fw-light">Admin Panel /</span> Service Updates
         </h4>
+
+        <form action="/admin/servicesupdates" method="POST">
+            @csrf
+            <div class="input-group">
+            <select name="action" class="form-select form-select-lg">
+                <option value="delete_all">Delete Selected Updates</option>
+                <option value="delete_description">Delete Selected Update Descriptions</option>
+                <option value="set_public">Set Selected Updates -> Public</option>
+                <option value="set_notpublic">Set Selected Updates -> Not Public</option>
+                <option value="set_pricepublic">Set Show Price Changes -> Public</option>
+                <option value="set_pricenotpublic">Set Show Price Changes -> Not Public</option>
+            </select>
+            <button type="submit" class="btn btn-primary">
+                <span class="tf-icons bx bx-right-arrow"></span>&nbsp; Submit
+            </button>
+            </div>
+        <div style="margin-top: 10px; margin-bottom: 20px;">
+        <input class="form-check-input me-1" type="checkbox" onclick="toggle(this);">Select / Unselect All Service Updates
+        </div>
         @if($errors->any())
             <div class="alert alert-danger">
                 <ul style="margin-bottom: 1px;">
@@ -28,14 +47,8 @@
             <div class="row">
                 <!-- List group checkbox -->
                 <div class="col-lg-12">
-                    @if(count($serviceupdates))
-                    <small class="text-light fw-semibold">The portion of the service updates that will be displayed to the user is set here.</small>
-                    @endif
                     <div class="demo-inline-spacing mt-3">
                         <div class="list-group">
-                            <form action="/admin/servicesupdates" method="POST">
-                            @csrf
-
                             @forelse($serviceupdates as $serviceupdate)
                                 @if(isset($serviceupdate->old_price) && isset($serviceupdate->new_price))
                                 <label class="list-group-item">
@@ -61,7 +74,6 @@
                                     <b>No Updates Found.</b>
                                 </center>
                             @endforelse
-                            </form>
                         </div>
                     </div>
                 </div>
@@ -69,5 +81,15 @@
             </div>
         </div>
     </div>
+    </form>
 
+    <script>
+        function toggle(source) {
+            var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            for (var i = 0; i < checkboxes.length; i++) {
+                if (checkboxes[i] != source)
+                    checkboxes[i].checked = source.checked;
+            }
+        }
+    </script>
 @endsection
