31da29c40a0fb412c2387bcd16ee26ba2456825c
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-15 14:12:57 +0300

    ticketPage func

--- app/Http/Controllers/admin/TicketController.php
+++ app/Http/Controllers/admin/TicketController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function ticketPage()
+    {
+        return view('pages.admin.tickets', [
+            'tickets' => Ticket::with('ticketMessages')->get()
+        ]);
+    }
 }
