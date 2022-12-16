a7db0c5276678f32dd387135aaf01038a2761c20
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-17 01:58:51 +0300

    ticketMessages func

--- app/Http/Controllers/admin/TicketController.php
+++ app/Http/Controllers/admin/TicketController.php
@@ -15,4 +15,15 @@ class TicketController extends Controller
             'tickets' => Ticket::with('ticketMessages', 'user')->get()
         ]);
     }
+
+    public function ticketMessages(Ticket $ticket)
+    {
+        TicketMessage::where('ticket_id', $ticket->id)->update(['seen_by_support' => 1]);
+
+        return view('pages.admin.ticket', [
+            'ticketMessages' => TicketMessage::with('owner')->where('ticket_id', $ticket->id)->orderBy('created_at', 'ASC')->get(),
+            'ticket_id' => $ticket->id,
+            'status' => $ticket->value('status')
+        ]);
+    }
 }
