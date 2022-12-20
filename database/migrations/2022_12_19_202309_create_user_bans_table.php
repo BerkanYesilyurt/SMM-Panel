04b03124bfdfb9e72149fca38441973098064a68
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-21 00:03:07 +0300

    create user_bans table

---
+++ database/migrations/2022_12_19_202309_create_user_bans_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_bans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('type');
+            $table->boolean('permanent');
+            $table->timestamp('until_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_bans');
+    }
+};
