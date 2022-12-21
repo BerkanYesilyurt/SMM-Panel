4f6b020e7f1ed6f8aee5e23a872597f3bff98c1b
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-22 01:43:59 +0300

    checkBan and getBanDateMessage helpers

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Enums\UserBanTypesEnum;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+if (!function_exists('checkBan')){
+    function checkBan($type):bool {
+        if(Auth::user()->is_banned){
+            $ban_record = Auth::user()->is_banned->where('type', UserBanTypesEnum::from($type)->value)->first();
+            if($ban_record){
+                if($ban_record->permanent || ($ban_record->until_at && Carbon::now()->lt($ban_record->until_at))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+if (!function_exists('getBanDateMessage')) {
+    function getBanDateMessage($type)
+    {
+        if (Auth::user()->is_banned) {
+            $ban_record = Auth::user()->is_banned->where('type', UserBanTypesEnum::from($type)->value)->first();
+            if ($ban_record) {
+                if($ban_record->permanent){
+                    return '<span class="badge bg-danger">There is no time limit for this ban.</span>';
+                }else{
+                    return 'Your account is temporarily suspended until <span class="badge bg-danger">' . date('d F Y h:i:s', strtotime($ban_record->until_at)) . '</span>';
+                }
+            }
+        }
+        return null;
+    }
+}
