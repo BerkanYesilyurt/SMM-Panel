399c609308446bc15df626c6b284bd6e8b32fe67
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-28 01:02:32 +0300

    checkBan helper restructured

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 if (!function_exists('checkBan')){
-    function checkBan($type):bool {
-        if(Auth::user()->is_banned){
-            $ban_record = Auth::user()->is_banned->where('type', UserBanTypesEnum::from($type)->value)->first();
+    function checkBan($type, $user = NULL):bool {
+        if(is_null($user)){
+            $user = Auth::user();
+        }
+
+        if($user->is_banned){
+            $ban_record = $user->is_banned->where('type', UserBanTypesEnum::from($type)->value)->first();
             if($ban_record){
                 if($ban_record->permanent || ($ban_record->until_at && Carbon::now()->lt($ban_record->until_at))){
                     return true;
@@ -27,7 +31,7 @@ if (!function_exists('getBanDateMessage')) {
                 if($ban_record->permanent){
                     return '<span class="badge bg-danger">There is no time limit for this ban.</span>';
                 }else{
-                    return 'Your account is temporarily suspended until <span class="badge bg-danger">' . date('d F Y h:i:s', strtotime($ban_record->until_at)) . '</span>';
+                    return 'The account is temporarily suspended until <span class="badge bg-danger">' . date('d F Y h:i:s', strtotime($ban_record->until_at)) . '</span>';
                 }
             }
         }
