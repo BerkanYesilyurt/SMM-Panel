7a6dbd8c71d915f7c561c5a65d62e734cd2d03a3
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-28 01:06:18 +0300

    UserBanTypesEnum values

--- app/Enums/UserBanTypesEnum.php
+++ app/Enums/UserBanTypesEnum.php
@@ -6,4 +6,9 @@ enum UserBanTypesEnum: string
 {
     case ticket = 'ticket';
     case account = 'account';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
