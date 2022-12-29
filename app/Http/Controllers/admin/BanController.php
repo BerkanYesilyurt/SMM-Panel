cc98b83f14fe1f3edd2db9dfefed97b6bd9e7a6c
BerkanYesilyurt <berkan38212@gmail.com>
2022-12-29 17:49:33 +0300

    banPage func

--- app/Http/Controllers/admin/BanController.php
+++ app/Http/Controllers/admin/BanController.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Enums\UserBanTypesEnum;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Enum;
 
 class BanController extends Controller
 {
-    //
+    public function banPage(User $user, $type)
+    {
+        return view('pages.admin.ban', [
+            'user' => $user,
+            'type' => in_array($type, UserBanTypesEnum::values()) ? $type : NULL
+        ]);
+    }
+
+    public function ban()
+    {
+
+    }
+
+    public function deleteBan()
+    {
+
+    }
 }
