ea0fb565f4c55fe0038cf1382add7672d203f74c
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-01 00:49:49 +0300

    deleteBan func

--- app/Http/Controllers/admin/BanController.php
+++ app/Http/Controllers/admin/BanController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\admin;
 use App\Enums\UserBanTypesEnum;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\UserBan;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Enum;
 
 class BanController extends Controller
@@ -23,8 +25,16 @@ class BanController extends Controller
 
     }
 
-    public function deleteBan()
+    public function deleteBan(Request $request)
     {
+        $request->validate([
+            'type' => new Enum(UserBanTypesEnum::class),
+            'user_id' => ['required', Rule::exists('user_bans','user_id')->where(function ($query) use ($request){
+                $query->where('type', $request->type);
+            })]
+        ]);
 
+        UserBan::where('user_id', $request->user_id)->where('type', $request->type)->delete();
+        return back()->with('message', 'You have successfully deleted the ban.');
     }
 }
