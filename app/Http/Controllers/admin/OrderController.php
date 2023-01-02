30dd9f0456dab005d206a7be7fdfe824bc2655dd
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-03 00:29:16 +0300

    ordersPage func

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function ordersPage()
     {
-        return view('pages.admin.orders');
+        return view('pages.admin.orders', [
+            'orders' => Order::with('getServiceName')->paginate(50),
+            'orderCount' => Order::count()
+        ]);
     }
 }
