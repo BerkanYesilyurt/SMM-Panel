56e46dc7bfb2bd4499476d207fe8c9f66dd923d3
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-07 19:52:39 +0300

    create payment methods table

---
+++ database/migrations/2023_01_07_161808_create_payment_methods_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enums\ActiveInactiveState;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('status')->default(ActiveInactiveState::ACTIVE->value);
+            $table->string('config_key')->nullable();
+            $table->string('config_value')->nullable();
+            $table->text('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+};
