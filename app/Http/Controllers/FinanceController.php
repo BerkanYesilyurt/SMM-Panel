dd037daef2eace5d7ca8feca496c638d59628cf2
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-09 00:11:33 +0300

    addFundsPage & paymentMethodPage funcs

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
 
 class FinanceController extends Controller
 {
     public function addFundsPage()
     {
-        return view('pages.addfunds');
+        return view('pages.addfunds', [
+            'paymentMethods' => PaymentMethod::all()
+        ]);
+    }
+
+    public function paymentMethodPage(PaymentMethod $paymentMethod)
+    {
+        return view('pages.payment-method', [
+            'paymentMethod' => $paymentMethod
+        ]);
     }
 }
