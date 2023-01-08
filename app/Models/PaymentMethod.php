26504495ad73c16a5064f42ce2f0f71cb908c8fd
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-08 18:44:14 +0300

    create PaymentMethod model

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\PaymentMethod
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentMethod query()
+ * @mixin \Eloquent
+ */
+class PaymentMethod extends Model
+{
+    protected $table = 'payment_methods';
+    protected $guarded = ['id'];
+}
