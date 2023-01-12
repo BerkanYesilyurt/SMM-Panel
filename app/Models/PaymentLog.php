30e87c7a3f37e341152f906206af56aa03f1f74b
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-13 00:30:29 +0300

    create PaymentLog model

---
+++ app/Models/PaymentLog.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\PaymentLog
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentLog newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentLog newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|PaymentLog query()
+ * @mixin \Eloquent
+ */
+
+class PaymentLog extends Model
+{
+    protected $table = 'payment_logs';
+    protected $guarded = ['id'];
+    protected $casts = [
+        'details' => 'array'
+    ];
+}
