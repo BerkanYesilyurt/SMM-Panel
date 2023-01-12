a14e08e457fb2c7a07afd7e88ba11bab48a2c0d4
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-13 00:30:41 +0300

    create payment_logs table

---
+++ database/migrations/2023_01_11_184811_create_payment_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enums\PaymentStatusEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('payment_method_id');
+            $table->string('currency');
+            $table->decimal('amount', 5, 2);
+            $table->text('details');
+            $table->string('status')->default(PaymentStatusEnum::PENDING->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_logs');
+    }
+};
