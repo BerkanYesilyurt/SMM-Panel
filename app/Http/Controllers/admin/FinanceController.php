4190a05562f1732d197ab8ccb43a316179e917ff
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-16 00:19:55 +0300

    paymentMethodsPage func

--- app/Http/Controllers/admin/FinanceController.php
+++ app/Http/Controllers/admin/FinanceController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\PaymentLog;
+use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
 
 class FinanceController extends Controller
 {
     public function paymentMethodsPage()
     {
-        return view('pages.admin.payment-methods');
+        return view('pages.admin.payment-methods', [
+            'paymentMethods' => PaymentMethod::all()
+        ]);
     }
 }
