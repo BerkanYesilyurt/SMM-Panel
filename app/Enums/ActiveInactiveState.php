2ecd6d0f3958df5b38ac32d77063a392237687b4
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-16 00:21:20 +0300

    ActiveInactiveState values

--- app/Enums/ActiveInactiveState.php
+++ app/Enums/ActiveInactiveState.php
@@ -5,4 +5,9 @@ namespace App\Enums;
 enum ActiveInactiveState:int {
     case INACTIVE = 0;
     case ACTIVE = 1;
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'name', 'value');
+    }
 }
