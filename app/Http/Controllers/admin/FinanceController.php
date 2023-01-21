df8b83b0874bb9f60a8a8080512f31244b05a95e
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-22 00:28:14 +0300

    createPaymentMethod func

--- app/Http/Controllers/admin/FinanceController.php
+++ app/Http/Controllers/admin/FinanceController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Enums\ActiveInactiveState;
 use App\Http\Controllers\Controller;
 use App\Models\PaymentLog;
 use App\Models\PaymentMethod;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Enum;
+use Str;
 
 class FinanceController extends Controller
 {
@@ -16,9 +19,24 @@ class FinanceController extends Controller
         ]);
     }
 
-    public function createPaymentMethod()
+    public function createPaymentMethod(Request $request)
     {
-        //TODO
+        $fields = $request->validate([
+            'name' => 'required|alpha|min:1|max:150',
+            'icon' => 'required|min:1|max:150',
+            'status' => new Enum(ActiveInactiveState::class),
+            'config_key' => 'nullable|min:1|max:150',
+            'config_value' => 'nullable|min:1|max:150',
+            'min_amount' => 'required|numeric|min:0.01|max:99999',
+            'max_amount' => 'required|numeric|min:0.01|max:99999|gte:min_amount',
+            'is_manual' => new Enum(ActiveInactiveState::class),
+            'content' => 'nullable|min:1|max:5000'
+        ]);
+
+        $fields['slug'] = Str::slug($request->name);
+        PaymentMethod::create($fields);
+
+        return back()->with('message', 'You have successfully created the payment method.');
     }
 
     public function updatePaymentMethod()
