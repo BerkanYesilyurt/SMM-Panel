7301ac6a04e9cd2bc569b8b711aba4839f5fef1c
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-24 00:09:00 +0300

    error message for invalid api requests

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,6 +44,12 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if($request->is('api/*')){
+                return response()->json(['error' => 'Incorrect request!'], 404);
+            }
+        });
+
         $this->reportable(function (Throwable $e) {
             //
         });
