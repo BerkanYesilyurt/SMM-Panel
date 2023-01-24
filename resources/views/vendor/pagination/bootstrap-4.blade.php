a1ff13158cfbbaec3fc3f68f30681e6d43755106
BerkanYesilyurt <berkan38212@gmail.com>
2023-01-25 00:11:06 +0300

    pagination style changes, first & last page links

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -1,12 +1,18 @@
 @if ($paginator->hasPages())
     <nav>
-        <ul class="pagination">
+        <ul class="pagination" style="justify-content: center;">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
+                <li class="page-item disabled" aria-disabled="true" aria-label="First Page">
+                    <span class="page-link" aria-hidden="true">&laquo;</span>
+                </li>
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
                     <span class="page-link" aria-hidden="true">&lsaquo;</span>
                 </li>
             @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ request()->url() }}" rel="prev" aria-label="First Page">&laquo;</a>
+                </li>
                 <li class="page-item">
                     <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
                 </li>
@@ -36,9 +42,15 @@
                 <li class="page-item">
                     <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
                 </li>
+                <li class="page-item">
+                    <a class="page-link" href="{{ request()->url().'?page='.$paginator->lastPage() }}" rel="next" aria-label="Last Page">&raquo;</a>
+                </li>
             @else
                 <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
                     <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                </li>
+                <li class="page-item disabled" aria-disabled="true" aria-label="Last Page">
+                    <span class="page-link" aria-hidden="true">&raquo;</span>
                 </li>
             @endif
         </ul>
