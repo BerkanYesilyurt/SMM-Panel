517776e43f36458f7de4ecd0d5b6153ce4bedb61
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-01 01:04:01 +0300

    DatabaseSeeder refactored

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Config;
+use App\Models\Faq;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
@@ -16,160 +17,117 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        DB::table('configs')->insert([
+        $configs = [
             [
                 'name' => 'is_installed',
-                'value' => '0',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '0'
             ],
             [
                 'name' => 'maintenance_mode',
-                'value' => '0',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '0'
             ],
             [
                 'name' => 'title',
-                'value' => 'SMM-Panel',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'SMM-Panel'
             ],
             [
                 'name' => 'meta_description',
-                'value' => 'SMM Panel is a professional panel that only offers High Quality services.',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'SMM Panel is a professional panel that only offers High Quality services.'
             ],
             [
                 'name' => 'meta_keywords',
-                'value' => 'smm panel, social media marketing, smm',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'smm panel, social media marketing, smm'
             ],
             [
                 'name' => 'website_logo',
-                'value' => '',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => ''
             ],
             [
                 'name' => 'website_favicon',
-                'value' => '',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => ''
             ],
             [
                 'name' => 'currency',
-                'value' => 'USD',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'USD'
             ],
             [
                 'name' => 'currency_symbol',
-                'value' => '$',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '$'
             ],
             [
                 'name' => 'service_updates_page',
-                'value' => '1',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '1'
             ],
             [
                 'name' => 'register_page',
-                'value' => '1',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '1'
             ],
             [
                 'name' => 'directly_login',
-                'value' => '1',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '1'
             ],
             [
                 'name' => 'autologin_after_registration',
-                'value' => '0',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '0'
             ],
             [
                 'name' => 'forgot_password',
-                'value' => '1',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => '1'
             ],
             [
                 'name' => 'terms_content',
                 'value' => 'We reserve the right to change these terms of service without notice. You are expected to read all terms of service before placing any order to insure you are up to date with any changes or any future changes.',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'name' => 'policy_content',
                 'value' => 'No refunds will be made to your payment method. After a deposit has been completed, there is no way to reverse it. You must use your balance',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'name' => 'javascript_embed_header',
-                'value' => '',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => ''
             ],
             [
                 'name' => 'javascript_embed_footer',
-                'value' => '',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => ''
             ],
             [
                 'name' => 'facebook_link',
-                'value' => 'https://www.facebook.com',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'https://www.facebook.com'
             ]
             ,
             [
                 'name' => 'twitter_link',
-                'value' => 'https://www.twitter.com',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'https://www.twitter.com'
             ],
             [
                 'name' => 'instagram_link',
-                'value' => 'https://www.instagram.com',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'value' => 'https://www.instagram.com'
             ]
             ,
             [
                 'name' => 'youtube_link',
-                'value' => 'https://www.youtube.com',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
-            ]
-        ]);
+                'value' => 'https://www.youtube.com'
+            ],
+        ];
 
-        DB::table('faq')->insert([
+        $faqs = [
             [
                 'question' => 'What is SMM Panel?',
                 'answer' => 'SMM panel, which is the short form of Social Media marketing panel is generally utilized concerning advertising organizations, brands, or ventures via web-based media. As you surely understand, Social media is the thing to get done where individuals from varying backgrounds associate for a considerable length of time.',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
                 'question' => 'What is Partial status?',
                 'answer' => 'Partial Status is when we partially refund the remains of an order. Sometimes for some reasons we are unable to deliver a full order, so we refund you the remaining undelivered amount.',
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ]
-        ]);
+        ];
 
+        collect($configs)->each(function ($config){
+            Config::create($config);
+        });
 
+        collect($faqs)->each(function ($faq){
+            Faq::create($faq);
+        });
     }
 }
