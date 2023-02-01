3cd8821810195de3bb556e2f3e60d475534ad571
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-02 02:12:44 +0300

    create error_logs table

---
+++ database/migrations/2023_02_01_190014_create_error_logs_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('error_logs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->string('user_ip')->nullable();
+            $table->text('user_agent')->nullable();
+            $table->string('method')->nullable();
+            $table->string('referer')->nullable();
+            $table->text('url')->nullable();
+            $table->text('message')->nullable();
+            $table->text('filename')->nullable();
+            $table->text('line')->nullable();
+            $table->text('trace')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('error_logs');
+    }
+};
