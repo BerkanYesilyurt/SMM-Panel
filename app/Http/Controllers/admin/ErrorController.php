cde74307fde871fc705582655317d3cf507cd535
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-06 01:56:04 +0300

    create ErrorController & errorLogsPage func

---
+++ app/Http/Controllers/admin/ErrorController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\ErrorLog;
+
+class ErrorController extends Controller
+{
+    public function errorLogsPage()
+    {
+        return view('pages.admin.errors', [
+            'errors' => ErrorLog::with('user')->paginate(50)
+        ]);
+    }
+}
