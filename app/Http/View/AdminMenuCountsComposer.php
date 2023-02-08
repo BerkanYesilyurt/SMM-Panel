94655f2859c22cff909764c021506b4d4223354a
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-09 00:58:34 +0300

    create AdminMenuCountsComposer

---
+++ app/Http/View/AdminMenuCountsComposer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\View;
+use Illuminate\View\View;
+class AdminMenuCountsComposer
+{
+    public function compose(View $view)
+    {
+        //TODO: menu counts
+        //$view->with('__menuCounts', []);
+    }
+}
