55e7240c73051f8ab43eaa4ba9909d6c44b39233
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-09 00:58:54 +0300

    create ContentServiceProvider & conditions

---
+++ app/Providers/ContentServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\ConfigController;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class ContentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if(hasTables(['announcements']))
+        {
+            $announcementArray = AnnouncementController::announcements();
+            view()->share('announcementArray', $announcementArray);
+        }
+        if(hasTables(['configs']))
+        {
+            $configsArray = ConfigController::configs();
+            view()->share('configsArray', $configsArray);
+        }
+        if(hasTables([
+            'announcements','categories','configs','error_logs','faq','orders',
+            'payment_methods','service_updates','services','tickets','users'
+        ]))
+        {
+            view()->composer('pages.admin.layout', 'App\Http\View\AdminMenuCountsComposer');
+        }
+    }
+}
