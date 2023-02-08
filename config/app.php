86bd45fb8bfa17bdecd2bf384a5263b6264ef6fc
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-09 00:59:16 +0300

    ContentServiceProvider added

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -181,9 +181,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\ConfigServiceProvider::class,
-        App\Providers\AnnouncementServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        App\Providers\ContentServiceProvider::class,
 
         /*
          * Package Service Providers...
