230968fa69a02c1e3e1ec0286c4945b842a02351
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-10 21:53:46 +0300

    compose func (admin menu count)

--- app/Http/View/AdminMenuCountsComposer.php
+++ app/Http/View/AdminMenuCountsComposer.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace App\Http\View;
+use App\Models\{Announcement, Category, ErrorLog, Faq, Order, PaymentMethod, Service, ServiceUpdate, Ticket, User};
+
 use Illuminate\View\View;
 class AdminMenuCountsComposer
 {
     public function compose(View $view)
     {
-        //TODO: menu counts
-        //$view->with('__menuCounts', []);
+        $view->with('__menuCounts', [
+            'users' => User::count(),
+            'orders' => Order::count(),
+            'categories' => Category::count(),
+            'services' => Service::count(),
+            'service_updates' => ServiceUpdate::count(),
+            'payment_methods' => PaymentMethod::count(),
+            'announcements' => Announcement::count(),
+            'tickets' => Ticket::count(),
+            'faq' => Faq::count(),
+            'errors' => ErrorLog::count()
+        ]);
     }
 }
