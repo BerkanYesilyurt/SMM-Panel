1bbedb63a82c10d0e2771a0975e7fcf74ff88333
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-12 03:39:09 +0300

    deletion period of error logs added into configs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -76,6 +76,10 @@ class DatabaseSeeder extends Seeder
                 'value' => '1'
             ],
             [
+                'name' => 'errorlogs_delete',
+                'value' => '7'
+            ],
+            [
                 'name' => 'terms_content',
                 'value' => 'We reserve the right to change these terms of service without notice. You are expected to read all terms of service before placing any order to insure you are up to date with any changes or any future changes.',
             ],
