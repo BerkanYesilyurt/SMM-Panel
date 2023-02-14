afae4f9ddf614a3e04945badfb3fbbcaa41cbb54
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-15 00:50:23 +0300

    create api_response_logs table

---
+++ database/migrations/2023_02_14_191105_create_api_response_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_response_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('order_id');
+            $table->unsignedInteger('api_id');
+            $table->text('response');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_response_logs');
+    }
+};
