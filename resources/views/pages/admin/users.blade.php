d2b36f0a44ec5221b3520efd70cce2cc5c11ef92
BerkanYesilyurt <berkan38212@gmail.com>
2023-02-20 01:25:36 +0300

    new user modal & messages added

--- resources/views/pages/admin/users.blade.php
+++ resources/views/pages/admin/users.blade.php
@@ -4,7 +4,28 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <h4 class="fw-bold py-3 mb-4">
             <span class="text-muted fw-light">Admin Panel /</span> Users
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalCenterNewUser" style="float:right;">
+                <span class="tf-icons bx bx-plus"></span>&nbsp; New User
+            </button>
         </h4>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul style="margin-bottom: 1px;">
+                    @foreach ($errors->all() as $error)
+                        <li><b>{{$error}}</b></li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if(session('message'))
+            <div class="alert alert-success alert-dismissible">
+                <ul style="margin-bottom: 1px; color:#478924;">
+                    <b>{{session('message')}}</b>
+                </ul>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                </button>
+            </div>
+        @endif
 
         <div class="card">
 
@@ -21,10 +42,10 @@
                     <thead>
                     <tr>
                         <th style="width: 5%;"><center>ID</center></th>
-                        <th style="width: 20%;"><center>NAME</center></th>
+                        <th style="width: 15%;"><center>NAME</center></th>
                         <th style="width: 20%;"><center>E-MAIL</center></th>
                         <th style="width: 10%;"><center>AUTHORITY</center></th>
-                        <th style="width: 10%;"><center>STATUS</center></th>
+                        <th style="width: 15%;"><center>STATUS</center></th>
                         <th style="width: 8%;"><center>BALANCE</center></th>
                         <th style="width: 12%;"><center>CREATED AT</center></th>
                         <th style="width: 15%;"><center>EDIT</center></th>
@@ -52,7 +73,7 @@
                         echo 'success';
                     }
                     @endphp me-1">{{$user->authority == \App\Enums\UserAuthorityEnum::none->value ? 'USER' : strtoupper(\App\Enums\UserAuthorityEnum::from($user->authority)->name)}}</span></center></td>
-                            <td><center>{{\App\Enums\UserStatusEnum::from($user->status)->name}}</center></td>
+                            <td><center>{{checkBan('account', $user) ? 'Account Banned' : (checkBan('ticket', $user) ? 'Ticket Banned' : 'ACTIVE')}}</center></td>
                             <td><center>{{$configsArray['currency_symbol']}}{{round($user->balance, 4)}}</center></td>
                             <td><center>{{$user->created_at->diffForHumans()}}</center></td>
                             <td><center>
@@ -77,4 +98,65 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="modalCenterNewUser" tabindex="-1" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">New User</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" id="form" action="/admin/new-user">
+                        @csrf
+
+                        <div class="col mb-3">
+                            <label for="name" class="form-label">Name:</label>
+                            <input type="text" id="name" name="name" class="form-control" placeholder="Berkan Yeşilyurt" required>
+                        </div>
+
+                        <div class="col mb-3">
+                            <label for="email" class="form-label">Email:</label>
+                            <input type="text" id="email" name="email" class="form-control" placeholder="berkan38212@gmail.com" required>
+                        </div>
+
+                        <div class="col mb-3">
+                            <label for="balance" class="form-label">Balance:</label>
+                            <input type="text" id="balance" name="balance" class="form-control" value="0" required>
+                        </div>
+
+                        <div class="col mb-3">
+                            <label for="password" class="form-label">Password:</label>
+                            <input type="text" id="password" name="password" class="form-control" placeholder="············" required>
+                        </div>
+
+                        <div class="col mb-3">
+                            <label for="contact" class="form-label">Contact:</label>
+                            <input type="text" id="contact" name="contact" class="form-control" placeholder="Contact" required>
+                        </div>
+
+                        <div class="col mb-3">
+                            <label for="authority" class="form-label">Authority:</label>
+                            <select id="authority" class="form-control" name="authority">
+                                @foreach(App\Enums\UserAuthorityEnum::values() as $key => $value)
+                                    <option value="{{$key}}">{{strtoupper($value)}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="col mb-3">
+                            <button class="btn btn-primary" onclick="submit(); this.disabled = true;" style="color: white; width: 100%;">Create User</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function submit(){
+            document.getElementById("form").submit();
+        }
+    </script>
 @endsection
