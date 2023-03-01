425a960681e4340060214f1c3af737bdc3f55f3e
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-02 01:56:37 +0300

    ticket funcs tied to ticket status config

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -14,11 +14,18 @@ class TicketController extends Controller
     public function __construct()
     {
         $this->middleware('isticketbanned');
+        $this->middleware(function($request, $next){
+            if(configValue('ticket_status')){
+                return $next($request);
+            }
+            return redirect('/');
+        });
     }
 
     public function index(Ticket $ticket){
         $tickets = $ticket->with('ticketMessages')
             ->where('user_id', auth()->user()->id)
+            ->orderBy('created_at')
             ->paginate(15);
         return view('pages.tickets', [
             'tickets' => $tickets,
@@ -27,41 +34,43 @@ class TicketController extends Controller
     }
 
     public function createTicket(CreateTicketRequest $request){
+        $openTicketCount = Ticket::where('user_id', auth()->user()->id)
+            ->where('status', TicketStatusEnum::ACTIVE->value)
+            ->count();
+        if($openTicketCount+1 <= configValue('max_open_ticket')){
+            $ticket = new Ticket();
+            $ticket->user_id = auth()->user()->id;
+            $ticket->subject = $request->subject;
+            $ticket->message = $request->message;
 
-        $ticket = new Ticket();
+            if($request->subject == 'order'){
+                $ticket->order_id = $request->orderid ?? NULL;
+                $ticket->order_request = $request->order_request ?? NULL;
+            }
 
-        $ticket->user_id = auth()->user()->id;
-        $ticket->subject = $request->subject;
-        $ticket->message = $request->message;
+            if($request->subject == 'payment'){
+                $ticket->pay_type = $request->payment ?? NULL;
+                $ticket->pay_id = $request->payid ?? NULL;
+            }
 
-        if($request->subject == 'order'){
-        $ticket->order_id = $request->orderid ?? NULL;
-        $ticket->order_request = $request->order_request ?? NULL;
+            if($request->subject == 'request'){
+                $ticket->feature_request = $request->feature_request ?? NULL;
+            }
+
+            $ticket->save();
+
+            $ticketMessages = new TicketMessage();
+            $ticketMessages->ticket_id = $ticket->id;
+            $ticketMessages->user_id = auth()->user()->id;
+            $ticketMessages->message = $request->message;
+            $ticketMessages->seen_by_user = 1;
+            $ticketMessages->seen_by_support = 0;
+            $ticketMessages->save();
+
+            return back()->with('message', 'You have successfully created a ticket.');
         }
-
-        if($request->subject == 'payment'){
-        $ticket->pay_type = $request->payment ?? NULL;
-        $ticket->pay_id = $request->payid ?? NULL;
-        }
-
-        if($request->subject == 'request'){
-            $ticket->feature_request = $request->feature_request ?? NULL;
-        }
-
-        $ticket->save();
-
-
-        $ticketMessages = new TicketMessage();
-        $ticketMessages->ticket_id = $ticket->id;
-        $ticketMessages->user_id = auth()->user()->id;
-        $ticketMessages->message = $request->message;
-        $ticketMessages->seen_by_user = 1;
-        $ticketMessages->seen_by_support = 0;
-        $ticketMessages->save();
-
-
-        alert()->success('Success!','You have successfully created a ticket.')->timerProgressBar();
-        return redirect('/tickets');
+        return back()->withErrors(["tooMuchTickets" => "You cannot create a new ticket because you have too many open tickets.
+        Please wait for your existing tickets to be answered."]);
     }
 
     public function ticketMessages(Ticket $ticket){
@@ -79,23 +88,22 @@ class TicketController extends Controller
     }
 
     public function newTicketMessage(Request $request, Ticket $ticket){
-        $request->validate([
-            'ticket_id'            => ['required', 'digits_between:1,10'],
-            'message'            => ['required', 'max:5000']
-        ]);
+            $request->validate([
+                'ticket_id'            => ['required', 'digits_between:1,10'],
+                'message'            => ['required', 'max:5000']
+            ]);
 
-        $relatedTicket = $ticket->where('id', $request->ticket_id)->first();
-        if(auth()->user()->id == $relatedTicket->user_id && $relatedTicket->status != TicketStatusEnum::CLOSED->value){
-        $ticketMessages = new TicketMessage();
-        $ticketMessages->ticket_id = $request->ticket_id;
-        $ticketMessages->user_id = auth()->user()->id;
-        $ticketMessages->message = $request->message;
-        $ticketMessages->seen_by_user = 1;
-        $ticketMessages->seen_by_support = 0;
-        $ticketMessages->save();
+            $relatedTicket = $ticket->where('id', $request->ticket_id)->first();
+            if(auth()->user()->id == $relatedTicket->user_id && $relatedTicket->status != TicketStatusEnum::CLOSED->value){
+            $ticketMessages = new TicketMessage();
+            $ticketMessages->ticket_id = $request->ticket_id;
+            $ticketMessages->user_id = auth()->user()->id;
+            $ticketMessages->message = $request->message;
+            $ticketMessages->seen_by_user = 1;
+            $ticketMessages->seen_by_support = 0;
+            $ticketMessages->save();
 
-        alert()->success('Success!','You have successfully sent the message.')->timerProgressBar();
-        return back();
+            return back()->with('message', 'You have successfully sent the message.');
         }else{
             return redirect('/');
         }
