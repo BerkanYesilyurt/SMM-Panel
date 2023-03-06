c3690527f6ddb21742b60176acd0c9e57ad2ca1c
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-07 00:11:34 +0300

    apiPage func

--- app/Http/Controllers/admin/ApiController.php
+++ app/Http/Controllers/admin/ApiController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Api;
 
 class ApiController extends Controller
 {
     public function apiPage(){
-        return view('pages.admin.api');
+        return view('pages.admin.api', [
+            'apis' => Api::all()
+        ]);
     }
 }
