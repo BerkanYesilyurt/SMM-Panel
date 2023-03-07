375b83eddc713ad1b71b8e80533a2da5bc22c190
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-08 00:05:32 +0300

    delete API modal & JS

--- resources/views/pages/admin/api.blade.php
+++ resources/views/pages/admin/api.blade.php
@@ -40,7 +40,9 @@
                             <td><center><b>{{$api->id}}</b></center></td>
                             <td style="white-space:pre-wrap; word-wrap:break-word;"><center>{{$api->name}}</center></td>
                            <td><center>
-                                    <!-- TODO: API admin buttons -->
+                                   <button type="button" onclick="prepareForDelete(this)" data-apiid="{{$api->id}}" data-apiname="{{$api->name}}" class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#modalCenterDeleteApi">
+                                       <span class="tf-icons bx bx-trash"></span>
+                                   </button>
                                 </center>
                             </td>
                         </tr>
@@ -59,4 +61,41 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="modalCenterDeleteApi" tabindex="-1" style="display: none;" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Delete API</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="POST" id="deleteapi" action="/admin/delete-api">
+                        @csrf
+                        <input type="hidden" name="delete_id" id="delete_id">
+
+                        <div class="col mb-3">
+                            <span id="prepareForDelete"></span>
+                        </div>
+
+                        <div class="col mb-3">
+                            <button class="btn btn-danger" onclick="deleteApi(); this.disabled = true;" style="color: white; width: 100%;">Delete API</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function prepareForDelete(element){
+            document.getElementById('delete_id').value = element.dataset.apiid;
+            document.getElementById('prepareForDelete').innerHTML = '<b>' + element.dataset.apiname + '(ID: ' + element.dataset.apiid + ') </b>and <b>all related services with this API</b> will be deleted. Are you sure?';
+        }
+
+        function deleteApi(){
+            document.getElementById("deleteapi").submit();
+        }
+    </script>
 @endsection
