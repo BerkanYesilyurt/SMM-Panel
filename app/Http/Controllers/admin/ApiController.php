954caed0d0f4414fbc9f59ea882615a864009ccf
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-08 00:06:10 +0300

    deleteApi func

--- app/Http/Controllers/admin/ApiController.php
+++ app/Http/Controllers/admin/ApiController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Api;
+use App\Models\Service;
+use DB;
+use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
@@ -11,5 +14,29 @@ class ApiController extends Controller
         return view('pages.admin.api', [
             'apis' => Api::all()
         ]);
+    }
+
+    public function createApi()
+    {
+        //TODO: createApi
+    }
+
+    public function updateApi()
+    {
+        //TODO: updateApi
+    }
+
+    public function deleteApi(Request $request)
+    {
+        $request->validate([
+            'delete_id' => 'required|numeric|exists:apis,id',
+        ]);
+
+        DB::transaction(function () use($request){
+            Api::findOrFail($request->delete_id)->delete();
+            Service::where('api_provider_id', '=', $request->delete_id)->delete();
+        });
+
+        return back()->with('message', 'You have successfully deleted API and related services.');
     }
 }
