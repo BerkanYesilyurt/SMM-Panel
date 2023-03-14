37f7e60cd62bfb0102663367297885f462266cb9
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-15 00:18:31 +0300

    APIs table redesigned

--- database/migrations/2023_02_14_191826_create_apis_table.php
+++ database/migrations/2023_02_14_191826_create_apis_table.php
@@ -16,16 +16,20 @@ return new class extends Migration
         Schema::create('apis', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('url');
             $table->text('key');
-            $table->string('action');
-            $table->text('order_endpoint');
-            $table->string('order_method');
-            $table->string('order_id_key');
-            $table->text('status_endpoint');
-            $table->text('status_method');
-            $table->string('status_key');
-            $table->string('start_counter_key');
-            $table->integer('remain_key');
+            $table->string('services_action');
+            $table->string('add_action');
+            $table->string('status_action');
+            $table->string('refill_action');
+            $table->string('refill_status_action');
+            $table->string('balance_action');
+            $table->string('service_key');
+            $table->string('link_key');
+            $table->string('quantity_key');
+            $table->string('order_key');
+            $table->string('orders_key');
+            $table->string('refill_key');
             $table->timestamps();
         });
     }
