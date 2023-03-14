408d8608f87f76a1a406de22dc83d18df74ae52f
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-15 00:30:32 +0300

    create api_request_parameters table

---
+++ database/migrations/2023_03_14_222805_create_api_request_parameters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('api_request_parameters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('api_id');
+            $table->string('remote_parameter_key');
+            $table->string('parameter_key');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('api_request_parameters');
+    }
+};
