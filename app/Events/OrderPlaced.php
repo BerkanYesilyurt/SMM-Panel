950facdf43fef44cb5b2bf5c2bec75e20fa0c6e9
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-21 00:50:20 +0300

    create OrderPlaced event

---
+++ app/Events/OrderPlaced.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderPlaced
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Order $order)
+    {
+    }
+
+}
