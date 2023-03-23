a348d37d34d70545e8de1e1e852d12d8c93b92ff
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-24 00:40:17 +0300

    create OrderStatus event

---
+++ app/Events/OrderStatus.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderStatus
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Order $order)
+    {
+        //
+    }
+}
