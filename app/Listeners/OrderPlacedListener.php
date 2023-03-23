bf2cfcf4f11be1ce773fe74cf15a9727d06c3a11
BerkanYesilyurt <berkan38212@gmail.com>
2023-03-23 21:02:42 +0300

    send order to reseller

--- app/Listeners/OrderPlacedListener.php
+++ app/Listeners/OrderPlacedListener.php
@@ -3,6 +3,10 @@
 namespace App\Listeners;
 
 use App\Events\OrderPlaced;
+use App\Models\Api;
+use App\Models\ApiResponseLog;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
 
 class OrderPlacedListener
 {
@@ -25,6 +29,33 @@ class OrderPlacedListener
     public function handle(OrderPlaced $event)
     {
         $order = $event->order;
-        //TODO: Send Order to Reseller
+        $api = Api::where('id', $event->order->api_provider_id)->first();
+
+        try{
+            // TODO: withoutVerifying() will be deleted
+            $response = Http::withoutVerifying()
+                ->acceptJson()
+                ->connectTimeout(15)
+                ->post($api->url, [
+                    'key' => $api->key,
+                    'action' => $api->add_action,
+                    $api->service_key => $order->api_service_id,
+                    $api->link_key => $order->link,
+                    $api->quantity_key => $order->quantity
+                ]);
+
+                ApiResponseLog::create([
+                    'order_id' => $order->id,
+                    'api_id' => $order->api_provider_id,
+                    'response' => $response->successful() ? $response->body() : 'ERROR: ' . $response->body()
+                ]);
+
+        }catch (ConnectionException $e){
+            ApiResponseLog::create([
+                'order_id' => $order->id,
+                'api_id' => $order->api_provider_id,
+                'response' => 'ERROR: ' . $e->getMessage()
+            ]);
+        }
     }
 }
