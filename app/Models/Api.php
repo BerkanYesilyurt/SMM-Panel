e18468487fccf9d26b0f3b6dac785a87bf589f37
BerkanYesilyurt <berkan38212@gmail.com>
2023-04-02 18:09:24 +0300

    lastBalance relation

--- app/Models/Api.php
+++ app/Models/Api.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use App\Enums\ApiResponseTypesEnum;
 use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Api
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|Api newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Api newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Api query()
+ * @mixin \Eloquent
+ */
 
 class Api extends Model
 {
     protected $table = 'apis';
     protected $guarded = ['id'];
+
+    public function lastBalance()
+    {
+        return $this->hasOne(ApiResponseLog::class, 'api_id', 'id')
+            ->where('type', ApiResponseTypesEnum::APIBALANCE->value)
+            ->latestOfMany();
+    }
 }
