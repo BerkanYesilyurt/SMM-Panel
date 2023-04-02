61bedfbde3e7d27de6c1a7aa04cd26e4080b98fc
BerkanYesilyurt <berkan38212@gmail.com>
2023-04-02 18:10:19 +0300

    response attribute

--- app/Models/ApiResponseLog.php
+++ app/Models/ApiResponseLog.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
+use App\Enums\ApiResponseTypesEnum;
 use Illuminate\Database\Eloquent\Model;
+use Str;
 
 class ApiResponseLog extends Model
 {
     protected $table = 'api_response_logs';
     protected $guarded = ['id'];
+
+    public function getResponseAttribute($value)
+    {
+        return Str::isJson($value) ? json_decode($value) : $value;
+    }
 
     public function api()
     {
