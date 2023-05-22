8a3708c1f1cb6f94c8d8a5982df9a9f1abde8b14
BerkanYesilyurt <berkan38212@gmail.com>
2023-05-22 21:05:11 +0300

    checkApiBalance func

--- app/Http/Controllers/admin/ApiController.php
+++ app/Http/Controllers/admin/ApiController.php
@@ -46,6 +46,11 @@ class ApiController extends Controller
         return back()->with('message', 'You have successfully edited API details.');
     }
 
+    public function checkApiBalance(Request $request)
+    {
+        //TODO
+    }
+
     public function deleteApi(Request $request)
     {
         $request->validate([
