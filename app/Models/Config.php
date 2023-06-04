5f9f7e5dbcec8b76d908d6a6be9c5a9f762f86cd
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-05 01:43:26 +0300

    guarded fields

--- app/Models/Config.php
+++ app/Models/Config.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -25,9 +24,5 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Config extends Model
 {
-
-    use HasFactory;
-
-    protected $guarded = ['id', 'name'];
-
+    protected $guarded = ['id'];
 }
