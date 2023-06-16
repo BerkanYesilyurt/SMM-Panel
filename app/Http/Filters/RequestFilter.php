d35f66ccbb400b504ff6e7c6c85f92b4eb1bddac
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-16 20:19:21 +0300

    filterByFunctions func

--- app/Http/Filters/RequestFilter.php
+++ app/Http/Filters/RequestFilter.php
@@ -2,7 +2,22 @@
 
 namespace App\Http\Filters;
 
+use Illuminate\Database\Eloquent\Builder;
+use Request;
+
 abstract class RequestFilter
 {
+    public function __construct(protected Request $request){}
 
+    public function filterByFunctions(Builder $builder): Builder
+    {
+        $this->builder = $builder;
+
+        foreach($this->request->all() as $name => $value) {
+            if(method_exists($this, $name)) {
+                call_user_func_array([$this, $name], [$value]);
+            }
+        }
+        return $this->builder;
+    }
 }
