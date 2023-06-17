df12a592b2b03b3b08ebba218976e4bedcda4337
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-17 19:31:58 +0300

    default filter func defined

--- app/Http/Filters/RequestFilter.php
+++ app/Http/Filters/RequestFilter.php
@@ -3,17 +3,17 @@
 namespace App\Http\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
-use Request;
 
 abstract class RequestFilter
 {
-    public function __construct(protected Request $request){}
-
+    public function default(){}
+    
     public function filterByFunctions(Builder $builder): Builder
     {
         $this->builder = $builder;
+        $this->default();
 
-        foreach($this->request->all() as $name => $value) {
+        foreach(request()->all() as $name => $value) {
             if(method_exists($this, $name)) {
                 call_user_func_array([$this, $name], [$value]);
             }
