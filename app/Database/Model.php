ca881a20b7b25d8fbe3eb4bce262af6a68e59e87
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-18 16:04:12 +0300

    overwritten model class

---
+++ app/Database/Model.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use App\Http\Filters\RequestFilter as Filter;
+
+abstract class Model extends EloquentModel
+{
+    /**
+     * @param Builder $query
+     * @param Filter $filter
+     * @return Builder
+     */
+    public function scopeFilterByFunctions(Builder $query, Filter $filter): Builder
+    {
+        return $filter->filterByFunctions($query);
+    }
+}
