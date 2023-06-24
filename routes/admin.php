004b43a1c99348a72c403fedc68d5395d65e17f1
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-25 00:34:08 +0300

    orders page routes defined (admin)

--- routes/admin.php
+++ routes/admin.php
@@ -55,7 +55,7 @@ Route::post('categories', [CategoryController::class, 'updateCategory']);
 Route::post('new-category', [CategoryController::class, 'createNewCategory']);
 Route::post('delete-category', [CategoryController::class, 'deleteCategory']);
 
-Route::get('orders', [OrderController::class, 'ordersPage']);
+Route::match(['POST', 'GET'], 'orders', [OrderController::class, 'ordersPage'])->name('admin-orders');
 
 Route::get('payment-methods', [FinanceController::class, 'paymentMethodsPage']);
 Route::post('payment-methods', [FinanceController::class, 'updatePaymentMethod']);
