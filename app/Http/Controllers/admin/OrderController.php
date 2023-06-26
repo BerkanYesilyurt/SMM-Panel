ec8c2e05acb88be57ae84af4bc8cd24a797c4b44
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-26 17:36:24 +0300

    ordersPage func (admin)

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\Enums\OrderStatusEnum;
 use App\Http\Controllers\Controller;
+use App\Http\Filters\OrdersPageFilter;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class OrderController extends Controller
 {
-    public function ordersPage()
+    public function ordersPage(Request $request, OrdersPageFilter $filter)
     {
+        $fields = $request->validate([
+            'status' => ['nullable', Rule::in(array_merge(OrderStatusEnum::getOnlyValues()->toArray(), ['all']))]
+        ]);
+
         return view('pages.admin.orders', [
-            'orders' => Order::with('getServiceName')->paginate(50),
-            'orderCount' => Order::count()
+            'statuses' => OrderStatusEnum::values(),
+            'orders' => Order::with('getServiceName')->filterByFunctions($filter)->paginate(50)->appends($fields),
+            'orderCount' => Order::filterByFunctions($filter)->count(),
+            'currentStatus' => $request->status ?? '-'
         ]);
     }
 }
