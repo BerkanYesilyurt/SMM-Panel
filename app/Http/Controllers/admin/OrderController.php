adf4920bda7e0531b22f4ef9ca5a15abf7e7b31b
BerkanYesilyurt <berkan38212@gmail.com>
2023-06-29 01:26:15 +0300

    check status & apply filters

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -6,22 +6,20 @@ use App\Enums\OrderStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\OrdersPageFilter;
 use App\Models\Order;
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class OrderController extends Controller
 {
-    public function ordersPage(Request $request, OrdersPageFilter $filter)
+    public function ordersPage(OrdersPageFilter $filter, $status = NULL)
     {
-        $fields = $request->validate([
-            'status' => ['nullable', Rule::in(array_merge(OrderStatusEnum::getOnlyValues()->toArray(), ['all']))]
-        ]);
+        if($status && !in_array($status, OrderStatusEnum::getOnlyNames(true)->toArray())){
+            return redirect()->route('admin-orders');
+        }
 
         return view('pages.admin.orders', [
             'statuses' => OrderStatusEnum::values(),
-            'orders' => Order::with('getServiceName')->filterByFunctions($filter)->paginate(50)->appends($fields),
-            'orderCount' => Order::filterByFunctions($filter)->count(),
-            'currentStatus' => $request->status ?? '-'
+            'orders' => Order::with('getServiceName')->filterByFunctions($filter, ['status' => $status])->paginate(50),
+            'orderCount' => Order::filterByFunctions($filter, ['status' => $status])->count(),
+            'currentStatus' => $status ?? '-'
         ]);
     }
 }
