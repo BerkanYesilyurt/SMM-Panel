be2ba3f91e9d0f045f054de468639c5df270272f
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-01 01:42:26 +0300

    ticket status filter

--- app/Http/Filters/TicketIndexFilter.php
+++ app/Http/Filters/TicketIndexFilter.php
@@ -10,7 +10,18 @@ class TicketIndexFilter extends RequestFilter
 {
     public function default()
     {
+        $this->status($this->additionalParams->status);
         return $this->builder->where('user_id', auth()->user()->id)->orderByDesc('created_at');
     }
 
+    public function status($value)
+    {
+        if($value && $value != 'all'){
+            $status = match ($value){
+                'closed' => TicketStatusEnum::CLOSED->value,
+                'active' => TicketStatusEnum::ACTIVE->value
+            };
+            return $this->builder->where('status', $status);
+        }
+    }
 }
