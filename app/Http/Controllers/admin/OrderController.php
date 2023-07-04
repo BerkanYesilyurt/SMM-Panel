0e5eda8017aedc95d19f0966d33bcac4c479943b
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-05 01:00:22 +0300

    validate search field

--- app/Http/Controllers/admin/OrderController.php
+++ app/Http/Controllers/admin/OrderController.php
@@ -6,14 +6,16 @@ use App\Enums\OrderStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Filters\OrdersPageFilter;
 use App\Models\Order;
+use Illuminate\Http\Request;
+
 
 class OrderController extends Controller
 {
-    public function ordersPage(OrdersPageFilter $filter, $status = NULL)
+    public function ordersPage(Request $request, OrdersPageFilter $filter, $status = NULL)
     {
-        if($status && !in_array($status, OrderStatusEnum::getOnlyNames(true)->toArray())){
-            return redirect()->route('admin-orders');
-        }
+        $request->validate([
+            'search' => 'nullable|min:1|max:500'
+        ]);
 
         return view('pages.admin.orders', [
             'statuses' => OrderStatusEnum::values(),
