776995ab1d6e6f72dff4021ac1c10d7f3e24a6c4
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-12 00:05:20 +0300

    ticket id added into search func & minor changes

--- app/Http/Filters/TicketIndexFilter.php
+++ app/Http/Filters/TicketIndexFilter.php
@@ -11,19 +11,20 @@ class TicketIndexFilter extends RequestFilter
     public function default()
     {
         $this->status($this->additionalParams->status);
-        return $this->builder->where('user_id', auth()->user()->id)->orderByDesc('created_at');
+        return $this->builder->orderByDesc('created_at');
     }
 
     public function search($value)
     {
-        return $this->builder->where('order_id', $value)
+        return $this->builder->where('id', $value)
+            ->orwhere('order_id', $value)
             ->orWhere('pay_id', $value)
             ->orWhere('message', 'LIKE', '%'.$value.'%');
     }
 
     public function status($value)
     {
-        if($value && $value != 'all'){
+        if($value && $value != 'all' && in_array($value, TicketStatusEnum::getOnlyNames(true)->toArray())){
             $status = match ($value){
                 'closed' => TicketStatusEnum::CLOSED->value,
                 'active' => TicketStatusEnum::ACTIVE->value
