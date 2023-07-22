2b0ec748fa68f381ee0a3331e83b75de43d2c07d
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-22 20:09:53 +0300

    search filter for users

--- app/Http/Filters/UsersPageFilter.php
+++ app/Http/Filters/UsersPageFilter.php
@@ -4,5 +4,12 @@ namespace App\Http\Filters;
 
 class UsersPageFilter extends RequestFilter
 {
-
+    public function search($value)
+    {
+        return $this->builder->where(function ($query) use ($value) {
+            $query->where('name', 'LIKE', '%'.$value.'%')
+                ->orWhere('email', 'LIKE', '%'.$value.'%')
+                ->orWhere('contact', 'LIKE', '%'.$value.'%');
+        });
+    }
 }
