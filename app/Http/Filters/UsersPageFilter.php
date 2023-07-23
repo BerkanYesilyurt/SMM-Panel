cb403f3cbfac8acac655ab5175db7f6ce8b8b7e2
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-23 15:54:03 +0300

    orderBy filter

--- app/Http/Filters/UsersPageFilter.php
+++ app/Http/Filters/UsersPageFilter.php
@@ -4,6 +4,14 @@ namespace App\Http\Filters;
 
 class UsersPageFilter extends RequestFilter
 {
+    public function orderby($value)
+    {
+        $explodedValue = explode('_', $value);
+        if(in_array($explodedValue[1], ['id', 'balance'])){
+            return $this->builder->orderBy($explodedValue[1], $explodedValue[0]);
+        }
+    }
+
     public function search($value)
     {
         return $this->builder->where(function ($query) use ($value) {
