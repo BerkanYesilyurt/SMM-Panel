64602bbbf88d415a0032b5b781b7b348f6823133
BerkanYesilyurt <berkan38212@gmail.com>
2023-07-27 21:04:01 +0300

    ID search added into filter

--- app/Http/Filters/UsersPageFilter.php
+++ app/Http/Filters/UsersPageFilter.php
@@ -15,7 +15,8 @@ class UsersPageFilter extends RequestFilter
     public function search($value)
     {
         return $this->builder->where(function ($query) use ($value) {
-            $query->where('name', 'LIKE', '%'.$value.'%')
+            $query->where('id', $value)
+                ->orWhere('name', 'LIKE', '%'.$value.'%')
                 ->orWhere('email', 'LIKE', '%'.$value.'%')
                 ->orWhere('contact', 'LIKE', '%'.$value.'%');
         });
